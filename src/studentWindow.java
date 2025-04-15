f02e85dc7903c13f720265cd6eb296510d7e3f5a
bmarkov25 <b.markov25@acsbg.org>
2025-04-15 10:20:55 +0300

    init studentWindow

---
+++ src/studentWindow.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class studentWindow {
+    private JPanel studentPanel;
+    private JTable grades;
+    private JButton gradeCalcButton;
+    private JLabel welcomeLabel;
+}
